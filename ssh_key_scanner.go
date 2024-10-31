e57bbf60c17e07a7da92dfc14f22f62d6b38385f
clearly-tyler-whitney <jake@kuz.ma>
2024-10-31 13:28:23 -0400

    working proof-of-concept

---
+++ ssh_key_scanner.go
@@ -0,0 +1,124 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"net"
+	"strings"
+	"sync"
+	"time"
+
+	"golang.org/x/crypto/ssh"
+)
+
+func main() {
+	// Define the --cidr flag
+	cidrStr := flag.String("cidr", "", "Comma-separated list of CIDRs to scan")
+	flag.Parse()
+
+	if *cidrStr == "" {
+		fmt.Println("Please provide CIDRs using --cidr flag")
+		return
+	}
+
+	// Split the CIDR string into a list
+	cidrList := strings.Split(*cidrStr, ",")
+	var ipList []string
+
+	// Expand each CIDR into individual IP addresses
+	for _, cidr := range cidrList {
+		cidr = strings.TrimSpace(cidr)
+		ips, err := hosts(cidr)
+		if err != nil {
+			fmt.Printf("Invalid CIDR %s: %v\n", cidr, err)
+			continue
+		}
+		ipList = append(ipList, ips...)
+	}
+
+	var wg sync.WaitGroup
+	var mu sync.Mutex
+	hostKeyMap := make(map[string][]string) // Map of host key fingerprints to IPs
+
+	// Concurrently scan each IP address
+	for _, ip := range ipList {
+		wg.Add(1)
+		go func(ip string) {
+			defer wg.Done()
+			hostKey, err := getHostKey(ip + ":22")
+			if err != nil {
+				return
+			}
+			if hostKey == nil {
+				return
+			}
+			fingerprint := ssh.FingerprintSHA256(hostKey)
+			mu.Lock()
+			hostKeyMap[fingerprint] = append(hostKeyMap[fingerprint], ip)
+			mu.Unlock()
+		}(ip)
+	}
+
+	wg.Wait()
+
+	// Identify and print duplicate host keys
+	for fingerprint, ips := range hostKeyMap {
+		if len(ips) > 1 {
+			fmt.Printf("Duplicate host key %s used by hosts: %v\n", fingerprint, ips)
+		}
+	}
+}
+
+// getHostKey connects to an SSH server and retrieves its host key
+func getHostKey(addr string) (ssh.PublicKey, error) {
+	var hostKey ssh.PublicKey
+
+	config := &ssh.ClientConfig{
+		User: "ignored",
+		HostKeyCallback: func(hostname string, remote net.Addr, key ssh.PublicKey) error {
+			hostKey = key
+			return nil
+		},
+		Auth:    []ssh.AuthMethod{},
+		Timeout: 5 * time.Second,
+	}
+
+	conn, err := ssh.Dial("tcp", addr, config)
+	if err != nil {
+		if strings.Contains(err.Error(), "unable to authenticate") {
+			return hostKey, nil // Authentication failed but host key is retrieved
+		}
+		return nil, err
+	}
+	defer conn.Close()
+
+	return hostKey, nil
+}
+
+// hosts generates a list of IP addresses from a CIDR notation
+func hosts(cidr string) ([]string, error) {
+	ip, ipnet, err := net.ParseCIDR(cidr)
+	if err != nil {
+		return nil, err
+	}
+
+	var ips []string
+	for ip := ip.Mask(ipnet.Mask); ipnet.Contains(ip); inc(ip) {
+		ips = append(ips, ip.String())
+	}
+	// Remove network and broadcast addresses
+	if len(ips) > 2 {
+		return ips[1 : len(ips)-1], nil
+	}
+	return ips, nil
+}
+
+// inc increments an IP address
+func inc(ip net.IP) {
+	for j := len(ip) - 1; j >= 0; j-- {
+		ip[j]++
+		if ip[j] > 0 {
+			break
+		}
+	}
+}
